2fb1d4aa02e907c134f88fbc914d8bcdd20bc6df
thecoderishi <rishirverma512@gmail.com>
2021-07-12 19:36:56 +0530

    vanilla js and scss

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frontend Mentor - Tip calculator app
+
+![Design preview for the Tip calculator app coding challenge](./design/desktop-preview.jpg)
---
+++ app.js
@@ -0,0 +1,103 @@
+const tipPerPerson = document.querySelector('#tipPerPerson')
+const totalPerPerson = document.querySelector('#totalPerPerson')
+const tipCustom = document.querySelector('#tipCustom')
+const tips = document.querySelectorAll('.tip-percent__tips__tip')
+const person = document.querySelector('.person')
+const button = document.querySelector('button')
+const totalBillAmount = document.querySelector('#amount')
+const totalPersons = document.querySelector('#people')
+
+
+let tip = 0,
+    people = 0,
+    amount = 0
+
+const buttonDisabled = () => {
+    button.disabled = true;
+}
+
+const buttonEnable = () => {
+    button.disabled = false;
+}
+
+const setValue = (tip, amount) => {
+    tipPerPerson.textContent = tip.toFixed(2)
+    totalPerPerson.textContent = amount.toFixed(2)
+}
+
+const setError = () => {
+    person.classList.add('error')
+}
+
+const removeError = () => {
+    person.classList.remove('error')
+}
+
+const tipCalc = () => {
+    buttonEnable()
+    if (!people || people === 0) {
+        setError()
+        return
+    }
+    removeError();
+    const tipAmount = amount * (tip / 100);
+    const actualAmount = (amount + tipAmount) / people;
+    const actualTip = tipAmount / people;
+    setValue(actualTip, actualAmount);
+}
+
+const resetTips = () => {
+    tips.forEach((tip) => {
+        tip.checked = false;
+    })
+}
+
+const resetCustomTip = () => {
+    tipCustom.value = ''
+}
+
+//Events
+tipCustom.addEventListener('input', (e) => {
+    if (e.target.value !== '') {
+        resetTips()
+        tip = parseFloat(e.target.value)
+    }
+})
+
+totalBillAmount.addEventListener('input', (e) => {
+    amount = parseFloat(e.target.value)
+    tipCalc();
+})
+
+totalPersons.addEventListener('input', (e) => {
+    people = parseFloat(e.target.value)
+    if (!people || people === 0) {
+        setError()
+        return;
+    }
+    tipCalc()
+})
+
+
+tips.forEach((tip) => {
+    tip.addEventListener('click', () => {
+        resetCustomTip();
+        tip = parseFloat(tip.value)
+        tipCalc()
+    })
+})
+
+const resetValues = () => {
+    tip = 0;
+    people = 0;
+    amount = 0;
+    setValue(0, 0);
+    totalBillAmount.value = '';
+    totalPersons.value = '';
+    resetCustomTip();
+    resetTips();
+    removeError();
+    buttonDisabled();
+}
+
+resetValues()
